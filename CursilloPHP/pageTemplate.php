590aba4bde9a4011a748b38bba81b759372bec5e
khris540 <khrisbryan09@gmail.com>
2016-11-25 23:16:13 -0500

    added php pages

---
+++ CursilloPHP/pageTemplate.php
@@ -0,0 +1,67 @@
+<!DOCTYPE HTML>
+<html lang="en">
+  <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=width=device-width, initial-scale=1">
+  <meta name="Hotel Management" content="Main page">
+      <!--<link rel="stylesheet" href="css/bootstrap.min.css">-->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+      <link rel="stylesheet" href="css/styles.css">
+  <title>Cursillo</title>
+  </head>
+  <body>
+     
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+        <button type="button" 
+                class="navbar-toggle collapsed" 
+                data-toggle="collapse"
+                data-target="#collapsemenu"
+                aria-expanded="false">
+            <span class="sr-only">Toggle Navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+      <a href="index.html" class="navbar-brand">
+       Cursillo
+      </a>
+    </div>
+    <div class="collapse navbar-collapse" id="collapsemenu">
+      <ul class="nav navbar-nav">
+      <li><a href="index.html">Home</a></li>
+      <li><a href="retreat.html">Retreats</a></li>
+      <li><a href=""></a></li>
+      <li><a href=""></a></li>
+    </ul>
+     <!--<ul class="nav navbar-nav navbar-right">
+        <li><a href="login.html"><span class="glyphicon glyphicon-log-in"></span> Login/Signup</a></li>
+      </ul>-->
+      </div>  
+  </div>
+</nav>     
+     
+<div class="content container">
+  <div class="row">
+    <section class="col-xs-12">
+    
+    
+    
+    
+     </section>
+  </div><!-- row -->   
+</div><!-- content container -->
+
+<footer class="footer">
+    <div class="container">
+        <p class="">&copy; 2016 Team1. | Databases</p>
+    </div>
+</footer>  
+    <script src="js/jquery-3.1.1.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!--<script src="js/bootstrap.min.js"></script>-->
+    <script src="js/script.js"></script>
+  </body>
+</html>
---
+++ CursilloPHP/search.php
@@ -0,0 +1,81 @@
+<!DOCTYPE HTML>
+<html lang="en">
+  <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=width=device-width, initial-scale=1">
+  <meta name="Hotel Management" content="Main page">
+      <!--<link rel="stylesheet" href="css/bootstrap.min.css">-->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+      <link rel="stylesheet" href="css/styles.css">
+  <title>Cursillo</title>
+  </head>
+  <body>
+     
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+        <button type="button" 
+                class="navbar-toggle collapsed" 
+                data-toggle="collapse"
+                data-target="#collapsemenu"
+                aria-expanded="false">
+            <span class="sr-only">Toggle Navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+      <a href="index.html" class="navbar-brand">
+       Cursillo
+      </a>
+    </div>
+    <div class="collapse navbar-collapse" id="collapsemenu">
+      <ul class="nav navbar-nav">
+      <li><a href="index.html">Home</a></li>
+      <li><a href="retreat.html">Retreats</a></li>
+      <li><a href="search.html">Search Database</a></li>
+      <li><a href="update.html">Update Database</a></li>
+    </ul>
+     <!--<ul class="nav navbar-nav navbar-right">
+        <li><a href="login.html"><span class="glyphicon glyphicon-log-in"></span> Login/Signup</a></li>
+      </ul>-->
+      </div>  
+  </div>
+</nav>     
+     
+<div class="content container">
+  <div class="row">
+    <section class="col-xs-12">
+    
+    <div class="dropdown" style="margin-top:100px;">
+        <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown" style=" height:50px;
+            width:150px;">Choose  
+        <span class="caret"></span></button>
+            <ul class="dropdown-menu">
+              <li><a href="#">Tables</a></li>
+              <li><a href="#">Columns</a></li>
+            </ul>
+    </div>
+    
+    <form action="">
+        <input type="text" style="margin-top:30px; height:50px; width:100%;">
+        
+        <button type="submit" class="button button-block" style="margin-top:25px; height:40px; width:125px;">Submit</button>
+    </form>
+    
+    
+     </section>
+  </div><!-- row -->   
+</div><!-- content container -->
+
+<footer class="footer">
+    <div class="container">
+        <p class="">&copy; 2016 Team1. | Databases</p>
+    </div>
+</footer>  
+    <script src="js/jquery-3.1.1.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!--<script src="js/bootstrap.min.js"></script>-->
+    <script src="js/script.js"></script>
+  </body>
+</html>
